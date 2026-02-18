9d138d0c2d4f1b015cb85c78437cd481b9aa373a
Russell Samora <russellsamora@gmail.com>
2026-02-18 09:30:54 -0500

    clean up

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -9,25 +9,19 @@
 	import Sewing from "$components/womens_sizes/Sewing.svelte";
 	import Outro from "$components/womens_sizes/Outro.svelte";
 	import Methods from "$components/womens_sizes/Methods.svelte";
-
-
 </script>
 
+<div class="grid-bg"></div>
+<Header />
+<Intro startStage={0} endStage={7} introScroll={true} />
+<TitlePage />
+<Intro startStage={8} />
+<SizeChart />
+<VanitySizes />
+<Sewing />
+<MassProduction />
+<Outro />
+<Methods />
 <svelte:boundary onerror={(e) => console.error(e)}>
-	<div class="grid-bg"></div>
-	<Header />
-	<Intro startStage={0} endStage={7} introScroll={true}/>
-	<TitlePage />
-	<Intro startStage={8}/>
-	<SizeChart />
-	<VanitySizes />
-	<Sewing />
-	<MassProduction />
-	<Outro />
-	<Methods />
 	<Footer recirc={true} />
 </svelte:boundary>
-
-<style>
-
-</style>
